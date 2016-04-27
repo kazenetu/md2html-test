0750b483c8f9f8f0399a911a212eb10cd7084773
kazenetu <rakudafactory@gmail.com>
2016-04-27 21:58:18 +0900

    テンプレート用pugを使って、MarkDownをhtmlに変換したファイルのhtml化

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 var gulp = require('gulp');
 var markdown = require('gulp-markdown');
 var pug = require('gulp-pug');
+var tap = require('gulp-tap');
+var path = require('path');
+var rename = require('gulp-rename');
 
 gulp.task('default',['_default'], function () {
-  return gulp.src('./lib/*.pug')
-    .pipe(pug({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./dist/'));
+  return gulp.src('./temp/*.html')
+      .pipe(tap(function(file, t){
+          return gulp.src('./lib/template.pug')
+            .pipe(pug({
+              pretty: true,
+              data:{
+                filedata:file.contents
+              }
+            }))
+            .pipe(rename(path.parse(file.path).base))
+            .pipe(gulp.dest('./dist/'));
+      }));
 });
 
 gulp.task('_default', function () {
-    return gulp.src('./sample-md/README.md')
+    return gulp.src('./sample-md/*.md')
         .pipe(markdown())
         .pipe(gulp.dest('./temp'));
 });
